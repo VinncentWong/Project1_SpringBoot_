45323fca3f7b27ff4f8bd8bb5119f19b07607b0d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-12 14:17:29 +0700

    add @GetMapping

--- src/main/java/com/project/controller/CheckHealth.java
+++ src/main/java/com/project/controller/CheckHealth.java
@@ -1,5 +1,6 @@
 package com.project.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/check")
 public class CheckHealth {
 	
+	@GetMapping("/health")
 	public String checkHealth() {
 		return "OK";
 	}
